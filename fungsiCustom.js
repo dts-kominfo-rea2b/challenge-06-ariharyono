5109f4a34e70917b5c55b00fb6c8aa5daabaf22c
Ari Haryono <33080550+ariharyono@users.noreply.github.com>
2022-06-10 20:05:24 +0700

    Update fungsiCustom.js

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -18,26 +18,52 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
+
 const dataOlah = (rawData) => {
   if (rawData.message !== undefined) {
     return rawData.message.split(" ")[1];
-  }
-  else if (rawData[0].message !== undefined) {
+  } else if (rawData[0].message !== undefined) {
     return rawData[0].message.split(" ")[1];
-  }
-  else if (rawData[0].data.message !== undefined) {
+  } else if (rawData[0].data.message !== undefined) {
     return rawData[0].data.message.split(" ")[1];
   }
 };
-const bacaData = async(callback) => {
-  let arrayFile = [file1, file2, file3];
-  let data = [];
-    for (const element of arrayFile) {
-      data.push(dataOlah(JSON.parse(await fs.promises.readFile(element))));
-    }
-    callback(null, data);
-};
+//tidak boleh menggunakan async await dan promise
+//sebaiknya callback tidak menggunakan forEach, map etc
+/*
+//const bacaData = async(callback) => {
+//  let arrayFile = [file1, file2, file3];
+//  let data = [];
+//    for (const element of arrayFile) {
+//      data.push(dataOlah(JSON.parse(await fs.promises.readFile(element))));
+//    }
+//    callback(null, data);
+//};
+*/
 
+const bacaData = (callback) => {
+  fs.readFile(file1, "utf-8", (err1, data1) => {
+    if (err1) {
+      return console.log(err1);
+    }
+
+    fs.readFile(file2, "utf-8", (err2, data2) => {
+      if (err2) {
+        return console.log(err2);
+      }
+
+      fs.readFile(file3, "utf-8", (err3, data3) => {
+        if (err3) {
+          return console.log(err3);
+        }
+        const olah1 = `['${dataOlah(JSON.parse(data1))}'`;
+        const olah2 = `'${dataOlah(JSON.parse(data2))}'`;
+        const olah3 = `'${dataOlah(JSON.parse(data3))}']`;
+        callback(err3, [olah1, olah2, olah3]);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
